87f4e8ad6c53506705068541f8d8ca104c7bc68f
David Maza Borjabad <109163247+d-maza@users.noreply.github.com>
2023-11-18 21:59:34 +0100

    add exercises

---
+++ js/lessons.js
@@ -0,0 +1,30 @@
+export const lessons = [
+  {
+    numLessons: 5,
+    issue: 'CSS3 Layout',
+    day: 52, // inicio 27 de Septiembre
+    msg: 'La copia puede ser un comienzo, pero la originalidad es el destino',
+    myProgress: '🥚>🐣>🐤>🐓>🕊️>🦃>🦅>🪂>🛩️>✈️>👨‍🚀>🚀>🛸>🦸‍♂️',
+
+    exercise: [
+      {
+        numExercise: 1,
+        description: `./assets/Ej1.png`,
+        urlCode:
+          'https://github.com/TECHMA-Bootcamp-FullStack-Java-Angular/dmb-tsys-html-1718-ta29/tree/main/Ejercicio_01',
+        urlPG: 'https://techma-bootcamp-fullstack-java-angular.github.io/dmb-tsys-html-1718-ta29/Ejercicio_01/index.html',
+     
+
+      },
+      {
+        numExercise: 2,
+        title: '',
+        description: `./assets/Ej2.png`,
+        urlCode:
+          'https://github.com/TECHMA-Bootcamp-FullStack-Java-Angular/dmb-tsys-html-1718-ta29/tree/main/Ejercicio_02',
+        urlPG: 'https://techma-bootcamp-fullstack-java-angular.github.io/dmb-tsys-html-1718-ta29/Ejercicio_02/instalaciones.html',
+      },
+
+    ],
+  },
+]
