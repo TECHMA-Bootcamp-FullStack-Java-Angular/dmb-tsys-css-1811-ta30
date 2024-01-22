6cc4bf557e385d71c57d2e8c520a00a4519cf376
David Maza Borjabad <109163247+d-maza@users.noreply.github.com>
2024-01-22 19:22:50 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,27 @@
+<div align="center"><img width="80%"  src="https://github.com/TECHMA-Bootcamp-FullStack-Java-Angular/dmb-tsys-java-2010-ta15/blob/main/docs/logoDark.png?raw=trueg"  alt="Logo BootCamp" /></div>
+
+# BootCamp T-Systems
+
+![My etiqueta](https://img.shields.io/badge/David%20Maza-DiveCode%F0%9F%90%99-blue) ![UD](https://img.shields.io/badge/TA-30-orange)  ![Java Version](https://img.shields.io/badge/CSS3-Layout-red)
+
+# T30 – CSS3 Layout
+
+This repository contains the Bootcamp tasks 👨‍💻
+
+[See exercise click here ](https://techma-bootcamp-fullstack-java-angular.github.io/dmb-tsys-css-1811-ta30/)  
+
+### Rules of use 🚀
+
+- Run the clone repository in your terminal:
+
+``git clone https://github.com/d-maza/dmb-tsys-css-1811-ta30``
+
+
+- Access the folder with the command:
+
+`cd dmb-tsys-css-1811-ta30`
+
+- Open the repository with your IDE
+
+
+
---
+++ js/lessons.js
@@ -0,0 +1,28 @@
+export const lessons = [
+  {
+    numLessons: 5,
+    issue: 'CSS3 Layout',
+    day: 52, // inicio 27 de Septiembre
+    msg: 'La copia puede ser un comienzo, pero la originalidad es el destino',
+    myProgress: '🥚>🐣>🐤>🐓>🕊️>🦃>🦅>🪂>🛩️>✈️>👨‍🚀>🚀>🛸>🦸‍♂️',
+
+    exercise: [
+      {
+        numExercise: 1,
+        description: `./assets/Ej1.png`,
+        urlCode:
+          'https://github.com/TECHMA-Bootcamp-FullStack-Java-Angular/dmb-tsys-css-1811-ta30/tree/main/Ejercicio_01',
+        urlPG: 'https://techma-bootcamp-fullstack-java-angular.github.io/dmb-tsys-css-1811-ta30/Ejercicio_01/index.html',
+      },
+      {
+        numExercise: 2,
+        title: '',
+        description: `./assets/Ej2.png`,
+        urlCode:
+          'https://github.com/TECHMA-Bootcamp-FullStack-Java-Angular/dmb-tsys-css-1811-ta30/tree/main/Ejercicio_02',
+        urlPG: 'https://techma-bootcamp-fullstack-java-angular.github.io/dmb-tsys-css-1811-ta30/Ejercicio_02/index.html',
+      },
+
+    ],
+  },
+]
